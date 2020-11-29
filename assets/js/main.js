0c0ebd7492a156a9dd637f4cf71bc6fb2965604b
lhenrique.dev <lhenrique.dev@gmail.com>
2020-11-29 19:03:02 -0300

    scroll and js stuffs :fire:

---
+++ assets/js/main.js
@@ -0,0 +1,50 @@
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  // validate that variables exist
+  if (toggle && nav) {
+    // we add the show-menu class to the div tag with nav__menu class
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show-menu");
+    });
+  }
+};
+showMenu("nav-toggle", "nav-menu");
+
+/*===== REMOVE MENU MOBILE =====*/
+
+const navLink = document.querySelectorAll(".nav__link");
+
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  // when we click on each nav__link, we remove the show-menu
+  navMenu.classList.remove("show-menu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*===== SCROLL SECTIONS ACTIVE LINK =====*/
+const sections = document.querySelectorAll("section[id]");
+
+window.addEventListener("scroll", scrollActive);
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active");
+    }
+  });
+}
